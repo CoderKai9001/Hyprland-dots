ae98dc00cb30339ab5edff13987bc2f7d71fdf99
CoderKai9001 <aditya.vadali@research.iiit.ac.in>
2026-05-15 23:15:18 +0530

    fixed lock keybing

--- keybinds.lua
+++ keybinds.lua
@@ -1,3 +1,5 @@
 -- This file will not be overwritten across dots-hyprland updates.
 -- The file name is for the sake of organization and does not matter
 -- See the corresponding files in ~/.config/hypr/hyprland for examples
+
+hl.bind("SUPER + L", hl.dsp.exec_cmd("qs -c ii ipc call lock activate"), { description = "Misc: Lock" })
